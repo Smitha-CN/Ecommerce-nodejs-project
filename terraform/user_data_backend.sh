ecbaad9e6cd3f15d7367045aa20759ab6f296694
Smitha-CN <smithamalthi@gmail.com>
2025-05-28 23:25:24 -0400

    Update user_data_backend.sh

--- terraform/user_data_backend.sh
+++ terraform/user_data_backend.sh
@@ -2,7 +2,7 @@
 yum update -y
 curl -sL https://rpm.nodesource.com/setup_18.x | bash -
 yum install -y nodejs git
-git clone https://github.com/your-user/your-ecomm-app.git /home/ec2-user/app
+git clone https://github.com/Smitha-CN/sample.git /home/ec2-user/app
 cd /home/ec2-user/app/backend
 npm install
 npm install -g pm2
