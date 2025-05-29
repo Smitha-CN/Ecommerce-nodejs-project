9cb8b097d11c25041f696eb39677ee299d0bca38
Smitha-CN <smithamalthi@gmail.com>
2025-05-29 16:11:36 -0400

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,21 @@
+
+
+variable "frontend_bucket_name" {
+  default = "ecomm-frontend-site-demo"
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+}
+
+variable "subnet_id" {
+  description = "Public subnet ID"
+}
+
+variable "key_name" {
+  description = "EC2 Key Pair"
+}
+
+variable "ec2_ami" {
+  default = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI (for example)
+}
