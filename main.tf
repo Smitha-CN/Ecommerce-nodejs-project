6affe7365c7cf455673d2b69e30427f9bcdc9334
Smitha-CN <smithamalthi@gmail.com>
2025-05-30 20:13:20 -0400

    Update main.tf
    rds

--- main.tf
+++ main.tf
@@ -87,3 +87,58 @@ resource "aws_instance" "backend_instance" {
     Name = "ecomm-backend"
   }
 }
+# rDS
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "rds-subnet-group"
+  subnet_ids = var.private_subnet_ids
+
+  tags = {
+    Name = "RDS Subnet Group"
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-sg"
+  description = "Allow MySQL access"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.backend_sg.id] # backend EC2 SG
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "RDS security group"
+  }
+}
+
+resource "aws_db_instance" "ecomm_rds" {
+  identifier              = "ecomm-db"
+  allocated_storage       = 20
+  storage_type            = "gp2"
+  engine                  = "mysql"
+  engine_version          = "8.0"
+  instance_class          = "db.t3.micro"
+  name                    = "ecommerce"
+  username                = var.db_username
+  password                = var.db_password
+  port                    = 3306
+  db_subnet_group_name    = aws_db_subnet_group.rds_subnet_group.name
+  vpc_security_group_ids  = [aws_security_group.rds_sg.id]
+  skip_final_snapshot     = true
+  publicly_accessible     = true
+
+  tags = {
+    Name = "ECommerce RDS DB"
+  }
+}
+
