b0b495c2b574fafa75d1daae03a22e0d3564615a
Smitha-CN <smithamalthi@gmail.com>
2025-06-02 20:07:25 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,21 @@ resource "aws_s3_bucket" "frontend_bucket" {
 }
 
 
+resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "PublicReadGetObject"
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "arn:aws:s3:::${var.frontend_bucket_name}/*"
+      }
+    ]
+  })
+}
 
 # CloudFront distribution for frontend
 
