4ef757dc599d402bcd4f9967f3f353bcfb3d86c4
Smitha-CN <smithamalthi@gmail.com>
2025-06-02 23:07:49 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,8 +30,73 @@ resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
     ]
   })
 }
+resource "aws_security_group" "backend_sg" {
+  name        = "backend-sg"
+  description = "Allow HTTP and SSH"
+  vpc_id      = var.vpc_id
 
-# CloudFront distribution for frontend
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# IAM Role for EC2
+resource "aws_iam_role" "ec2_role" {
+  name = "ecomm-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action    = "sts:AssumeRole",
+        Effect    = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_attach" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+s
+
+data "aws_route53_zone" "primary" {
+  name = "smithaproperties.com"
+}
+
+resource "aws_route53_record" "frontend_alias" {
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = "www.smithaproperties.com"
+  type    = "A"
+
+  alias {
+    name                   = "s3-website.ca-central-1.amazonaws.com" # Your region’s website endpoint
+    zone_id                = "Z1QDHH18159H29"                        # S3 Website endpoint Zone ID for ca-central-1
+    evaluate_target_health = false
+  }
+}
 
 
 
